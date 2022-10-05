146d9406fb76e6b81b511a12413a2f08dff9081c
Sergio Diosdado <sgdc_98@hotmail.com>
2022-10-05 17:22:59 -0500

    Export default app

--- api/index.ts
+++ api/index.ts
@@ -5,22 +5,19 @@ import path from 'path';
 
 dotenv.config();
 
-export const app = express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('./static'))
-
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: path.join(__dirname, 'static')});
-})
+app.use('/', express.static('./public'))
 
 app.get('/api/hello', async (request: Request, response: Response) => {
   response.json({"data": "Hello, world"})
 })
 
 const PORT = process.env.PORT || 3001
-export const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+export default app;
