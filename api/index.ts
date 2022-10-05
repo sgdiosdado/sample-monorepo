787bf59fe8e9e010be9d6b3e1b6707d11fcb6b09
Sergio Diosdado <sgdc_98@hotmail.com>
2022-10-05 17:07:27 -0500

    Add vercel config file to api package

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
@@ -9,6 +10,11 @@ export const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('./static'))
+
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, 'static')});
+})
 
 app.get('/api/hello', async (request: Request, response: Response) => {
   response.json({"data": "Hello, world"})
